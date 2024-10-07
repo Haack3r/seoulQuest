06f242e5f9ec85a0465e9be118e2ea7cd557ff37
deelee0122 <deelee0122@gmail.com>
2024-10-07 19:11:16 +0900

    update frontend

--- seoulQuestFrontend/src/components/menus/BasicMenu.js
+++ seoulQuestFrontend/src/components/menus/BasicMenu.js
@@ -1,16 +1,33 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
+import {
+    ShoppingBagIcon,
+    UserIcon,
+    LogInIcon,
+    LogOutIcon,
+    SearchIcon,
+    MenuIcon,
+    ChevronLeftIcon,
+    ChevronRightIcon,
+  } from "lucide-react";
 
 const BasicMenu = () => {
   const loginState = useSelector(state => state.loginSlice)
 
   return (
-    <header className="bg-white shadow-sm">
-      <div className="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8 flex justify-between items-center">
-        <Link to={'/'} className="text-2xl font-serif font-bold text-gray-900">Maison de Luxe</Link>
+    <header className="min-h-screen bg-white font-sans shadow-sm">
+          <div className="fixed top-0 left-0 right-0 bg-white bg-opacity-70 backdrop-blur-md z-50 ">
+          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex justify-between h-16 items-center">
+            <div className="flex items-center">
+              <Link to={'/'} className="text-2xl font-semibold tracking-wide text-gray-900">
+                Seoul<span className="text-orange-500">Culture</span>Quest
+              </Link>
+            </div>
         <nav>
-          <ul className="flex space-x-4">
+                          <ul className="flex space-x-4">
+                              <li><ShoppingBagIcon className="h-5 w-5" /></li>
             <li><Link to="/" className="text-gray-600 hover:text-gray-900">Home</Link></li>
             <li><Link to={'/products/'} className="text-gray-600 hover:text-gray-900">Products</Link></li>
       {!loginState.email ?
@@ -24,8 +41,11 @@ const BasicMenu = () => {
         </>}
         </ul>
         </nav>
-        </div>
+                  </div>
+              </div>
+              </div>
     </header>
+    
   )
 }
 
--- seoulQuestFrontend/src/pages/MainPage.js
+++ seoulQuestFrontend/src/pages/MainPage.js
@@ -4,10 +4,14 @@ import BasicLayout from '../layouts/BasicLayout';
 import ListComponent from '../components/products/ListComponent'
 import MainImage from '../layouts/MainImage';
 import SeoulCultureQuest from './SeoulCultureQuest';
+import BasicMenu from '../components/menus/BasicMenu';
 
 const MainPage = () => {
-  return (
-    <SeoulCultureQuest />
+    return (
+        <>
+            <SeoulCultureQuest />
+            {/* <BasicMenu /> */}
+            </>
     // <BasicLayout>
     //   <MainImage />
     // </BasicLayout>
