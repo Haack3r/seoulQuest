c4c4812e420aa7959ab151ab015e962e6fe5b4be
deelee0122 <deelee0122@gmail.com>
2024-10-27 20:12:56 +0900

    design: 메인페이지 디자인 수정

--- reactShoppingBackend/goodmorning/src/main/java/com/green/jaeyoon/goodmorning/util/CustomJWTException.java
+++
@@ -1,7 +0,0 @@
-package com.green.jaeyoon.goodmorning.util;
-
-public class CustomJWTException extends RuntimeException{
-    public CustomJWTException(String message) {
-        super(message);
-    }
-}
