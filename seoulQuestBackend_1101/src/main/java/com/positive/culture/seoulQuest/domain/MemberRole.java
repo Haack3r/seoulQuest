429415e863c512122744ac7672ab24a7ff4582ae
sjahn <sjahnver843@naver.com>
2024-11-05 10:09:57 +0900

    docs: making AdminController

--- seoulQuestBackend_1101/src/main/java/com/positive/culture/seoulQuest/domain/MemberRole.java
+++ seoulQuestBackend_1101/src/main/java/com/positive/culture/seoulQuest/domain/MemberRole.java
@@ -1,5 +1,14 @@
 package com.positive.culture.seoulQuest.domain;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum MemberRole {
-    USER, ADMIN; // 숫자 (USER = 0, ADMIN = 1, MANAGER = 2)
+    USER("ROLE_USER", "유저"),
+    ADMIN("ROLE_ADMIN", "관리자"); // 숫자 (USER = 0, ADMIN = 1)
+
+    private final String key;
+    private final String title;
 }
