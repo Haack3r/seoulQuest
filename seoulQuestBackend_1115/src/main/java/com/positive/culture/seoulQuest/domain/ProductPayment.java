307784587574b88bdef4e0eabc1b3a14ae39f808
Haileyhaha <hyeny718@gmail.com>
2024-11-15 18:15:31 +0900

    2024-11-15 수정

---
+++ seoulQuestBackend_1115/src/main/java/com/positive/culture/seoulQuest/domain/ProductPayment.java
@@ -0,0 +1,38 @@
+package com.positive.culture.seoulQuest.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+@Getter
+@Entity
+@Table(name = "tbl_product_payment")
+public class ProductPayment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long pPaymentId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn
+    private Member pPaymentMember;
+
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    private ProductOrder productOrder;
+
+    private String merchantUid;
+
+    private BigDecimal paymentPrice;
+    private Date paymentDate;
+    private String paymentStatus;
+
+    @Builder.Default
+    private String paymentMethod= "pending";
+}
