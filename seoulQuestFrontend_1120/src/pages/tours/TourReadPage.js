afdf845e6a63a5d7e1a5c27663b906b53c1f8da0
deelee0122 <deelee0122@gmail.com>
2024-11-24 00:40:56 +0900

    modify: Tour & Product 상세페이지 디자인 수정

--- seoulQuestFrontend_1120/src/pages/tours/TourReadPage.js
+++ seoulQuestFrontend_1120/src/pages/tours/TourReadPage.js
@@ -7,7 +7,7 @@ const TourReadPage = () => {
   const {tno} = useParams()
   
   return (
-    <div className='p-4 w-full bg-white'>
+    <div className='p-4 w-full'>
       <TourReadComponent tno={tno}></TourReadComponent>
     </div>
   )
