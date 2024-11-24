69754ad57401ec69328f5a628b056e0bda138987
hyeny718@gmail.com <hyeny718@gmail.com>
2024-11-24 21:34:10 +0900

    product , tour review 구현 완료

--- seoulQuestBackend_1122/src/main/java/com/positive/culture/seoulQuest/repository/TourReviewRepository.java
+++
@@ -1,7 +0,0 @@
-package com.positive.culture.seoulQuest.repository;
-
-import com.positive.culture.seoulQuest.domain.TourReview;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TourReviewRepository extends JpaRepository<TourReview,Long> {
-}
