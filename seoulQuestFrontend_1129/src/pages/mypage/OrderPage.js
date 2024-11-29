87a06f27fa5c4ed89d8f0d8c22a668a792e99265
deelee0122 <deelee0122@gmail.com>
2024-11-29 19:03:52 +0900

    modify: OrderPage 디자인 수정

--- seoulQuestFrontend_1129/src/pages/mypage/OrderPage.js
+++ seoulQuestFrontend_1129/src/pages/mypage/OrderPage.js
@@ -7,12 +7,17 @@ import OrderComponent from "../../components/member/OrderComponent";
 const OrderPage = () => {
   return (
     <BasicLayout>
-    <div className="p-6 bg-gray-100 flex justify-center items-center min-h-screen">
-     <div className="grid grid-cols-3 gap-6 w-full max-w-6xl mt-20">
-       <div className="col-span-1 min-h-screen max-h-screen bg-gray-100">
+    <div className=" bg-gray-100 flex justify-center items-start min-h-screen mt-14">
+     <div className="w-full max-w-6xl flex flex-col lg:flex-row gap-6 mt-20">
+       <div className="hidden lg:block w-1/3">
          <MyPageLayout/>
        </div>
-       <div className="col-span-2">
+        {/* Horizontal Menu for Small Screens */}
+        <div className="lg:hidden absolute top-16 left-0 w-full ">
+            <MyPageLayout />
+          </div>
+
+       <div className="w-full lg:w-2/3 p-6">
          <OrderComponent/>
        </div>
      </div>
