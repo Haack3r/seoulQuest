ecf63eb3c6cd574f5371c44bb8ca247f0e32d164
hyeny718@gmail.com <hyeny718@gmail.com>
2024-12-03 02:44:21 +0900

    tourDate 엔티티에서 @ElementCollection로 변경한 ver, front AvailableCapacity화면에 표시 및 Tour ReadPage디자인 추가

---
+++ seoulQuestBackend_1203/src/main/java/com/positive/culture/seoulQuest/domain/TourDate.java
@@ -0,0 +1,40 @@
+package com.positive.culture.seoulQuest.domain;
+
+// import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+// @Entity
+// @Table(name = "tbl_tour_date")
+@Embeddable
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class TourDate {
+
+    // @Id
+    // @GeneratedValue(strategy = GenerationType.IDENTITY)
+    // @Column(name = "tour_date_id")
+//    private long tourDate_id;
+
+    // @ManyToOne(fetch = FetchType.LAZY)
+    // @JoinColumn(name = "tour_no")
+    // @JsonBackReference
+    // private Tour tour;
+
+    private LocalDate tourDate;
+    private int availableCapacity;
+
+    public void changeTourDate(LocalDate tourDate) {
+        this.tourDate = tourDate;
+    }
+
+    public void changeAvailableCapacity(int availableCapacity) {
+        this.availableCapacity = availableCapacity;
+    }
+
+}
