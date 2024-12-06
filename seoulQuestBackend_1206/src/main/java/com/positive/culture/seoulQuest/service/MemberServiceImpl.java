459990b89bca60dca87d30258d6ea1d7fa8fb787
deelee0122 <deelee0122@gmail.com>
2024-12-06 18:27:15 +0900

    merge

---
+++ seoulQuestBackend_1206/src/main/java/com/positive/culture/seoulQuest/service/MemberServiceImpl.java
@@ -0,0 +1,276 @@
+package com.positive.culture.seoulQuest.service;
+
+import com.positive.culture.seoulQuest.domain.Address;
+import com.positive.culture.seoulQuest.domain.Member;
+import com.positive.culture.seoulQuest.domain.MemberRole;
+import com.positive.culture.seoulQuest.dto.CustomerListDTO;
+import com.positive.culture.seoulQuest.dto.MemberDTO;
+import com.positive.culture.seoulQuest.dto.MemberModifyDTO;
+import com.positive.culture.seoulQuest.dto.UserDTO;
+import com.positive.culture.seoulQuest.repository.MemberRepository;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Log4j2
+public class MemberServiceImpl implements MemberService {
+
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JavaMailSender mailSender;
+
+    @Value("${admin.name}")
+    private String adminName;
+
+    //구글 정책상 admin email을 지정해도 spring.mail.username 으로 사용됨.
+    @Value("${spring.mail.username}")
+    private String mailUsername;
+
+    @Override
+    public MemberDTO getKakaoMember(String accessToken) {
+        String email = getEmailFromKakaoAccessToken(accessToken);
+        log.info("email: " + email);
+        Optional<Member> result = memberRepository.findByEmail(email);
+
+        if (result.isPresent()) {
+            MemberDTO memberDTO = entityToDTO(result.get());
+            return memberDTO;
+        }
+        Member socialMember = makeSocialMember(email);
+        memberRepository.save(socialMember);
+        MemberDTO memberDTO = entityToDTO(socialMember);
+        return memberDTO;
+    }
+
+    @Override
+    public void modifyMember(MemberModifyDTO memberModifyDTO) {
+        Optional<Member> result = memberRepository.findByEmail(memberModifyDTO.getEmail());
+        Member member = result.orElseThrow();
+        member.changePw(passwordEncoder.encode(memberModifyDTO.getPw()));
+        member.changeSocial(false);
+        member.changeNick(memberModifyDTO.getNickname());
+
+        memberRepository.save(member);
+    }
+
+    @Override
+    public Optional<Member> findByEmail(String email) {
+        System.out.println("서비스  email:" + email);
+
+        return memberRepository.findByEmail(email);
+    }
+
+    @Override
+    public Optional<Member> findByNickname(String nickName) {
+        System.out.println("서비스  nickname:" + nickName);
+        return memberRepository.findByNickName(nickName);
+    }
+
+    @Override
+    public Member save(UserDTO dto) {
+
+        // Assemble phone number, handling any missing parts
+        String phoneNumber = String.join("-",
+                dto.getPhoneNumber1(),
+                dto.getPhoneNumber2(),
+                dto.getPhoneNumber3());
+
+        // Create a new Member instance
+        Member member = Member.builder()
+                .firstname(dto.getFirstname())
+                .lastname(dto.getLastname())
+                .nickName(dto.getNickName())
+                .email(dto.getEmail())
+                .phoneNumber(phoneNumber.trim())
+                .birthday(dto.getBirthday()) // Default to today if missing
+                .address(new Address(
+                        dto.getStreet(),
+                        dto.getCity(),
+                        dto.getState(),
+                        dto.getZipcode(),
+                        dto.getCountry()))
+                .password(passwordEncoder.encode(dto.getPassword()))
+                .build();
+
+        member.addRole(MemberRole.USER);
+
+        return memberRepository.save(member);
+    }
+
+    // userDTO받아서 member(entity)로 변환하고 repository저장
+    @Override
+    public void modifyInfo(UserDTO dto) {
+
+        String email = dto.getEmail();
+        Member member = memberRepository.findByEmail(email).orElseThrow();
+
+        String phoneNumber = String.join("-",
+                dto.getPhoneNumber1(),
+                dto.getPhoneNumber2(),
+                dto.getPhoneNumber3());
+
+        member.changeNick(dto.getNickName());
+        member.changePhone(phoneNumber);
+        member.changeAddress(new Address(
+                dto.getStreet(),
+                dto.getCity(),
+                dto.getState(),
+                dto.getZipcode(),
+                dto.getCountry()));
+        member.changePw(passwordEncoder.encode(dto.getPassword()));
+
+        memberRepository.save(member);
+
+    }
+
+    @Override
+    public Member findPasswordAndSendEmail(String email, String phoneNumber) {
+
+        Member member = memberRepository.findByEmailAndPhoneNumber(email, phoneNumber).orElseThrow();
+
+        // 1. 임시 비밀번호 생성
+        String tempPassword = makeTempPassword();
+        log.info("Temporary Password: " + tempPassword);
+
+        // 2. 임시 비밀번호 저장
+        member.changePw(passwordEncoder.encode(tempPassword));
+        memberRepository.save(member);
+
+        // 3. 이메일 발송
+        sendTemporaryPasswordEmail(member.getEmail(), tempPassword);
+
+        return member;
+    }
+
+    @Override
+    public Member findEmail(String firstname, String lastname, String phonenumber) {
+        Member member = memberRepository.findByFirstnameAndLastnameAndPhoneNumber(firstname, lastname, phonenumber)
+                .orElseThrow();
+        log.info(member);
+
+        return member;
+    }
+
+    private void sendTemporaryPasswordEmail(String toEmail, String tempPassword) {
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+
+            helper.setTo(toEmail);
+            helper.setFrom(String.format("%s <%s>", adminName, mailUsername));
+            helper.setSubject("Your Temporary Password");
+            // HTML 형식으로 이메일 내용 작성
+            String emailContent = String.format(
+                    "Hello,<br><br>Your temporary password is: <b>%s</b><br><br>Please change it after logging in.",
+                    tempPassword
+            );
+
+            helper.setText(emailContent, true); // HTML 형식으로 설정
+
+            mailSender.send(message);
+            log.info("Temporary password email sent to: " + toEmail);
+        } catch (MessagingException e) {
+            log.error("Failed to send email", e);
+            throw new RuntimeException("Failed to send temporary password email", e);
+        }
+    }
+
+    // 임시 비밀번호 발급시 사용함.
+    private String makeTempPassword() {
+        StringBuffer buffer = new StringBuffer();
+
+        for (int i = 0; i < 10; i++) {
+            buffer.append((char) ((int) (Math.random() * 55) + 65));
+        }
+        return buffer.toString();
+    }
+
+    private String getEmailFromKakaoAccessToken(String accessToken) {
+        String KakaoGetUserURL = "https://kapi.kakao.com/v2/user/me";
+
+        if (accessToken == null) {
+            throw new RuntimeException("Access token is null");
+        }
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Bearer " + accessToken);
+        headers.add("Content-Type", "application/x-www-form-urlencoded");
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        UriComponents uriBuilder = UriComponentsBuilder.fromHttpUrl(KakaoGetUserURL).build();
+
+        ResponseEntity<LinkedHashMap> response = restTemplate.exchange(
+                uriBuilder.toString(),
+                HttpMethod.GET,
+                entity,
+                LinkedHashMap.class);
+        log.info(response);
+
+        LinkedHashMap<String, LinkedHashMap> bodyMap = response.getBody();
+
+        log.info("---------------------");
+        log.info(bodyMap);
+
+        LinkedHashMap<String, String> kakaoAccount = bodyMap.get("kakao_account");
+
+        log.info("KakaoAccount: " + kakaoAccount);
+        return kakaoAccount.get("email");
+
+    }
+
+    private Member makeSocialMember(String email) {
+        String tempPassword = makeTempPassword();
+        log.info("tempPassword: " + tempPassword);
+        String nickname = "소셜회원";
+        Member member = Member.builder()
+                .email(email)
+                .password(passwordEncoder.encode(tempPassword))
+                .nickName(nickname)
+                .social(true)
+                .build();
+
+        member.addRole(MemberRole.USER);
+        return member;
+    }
+
+    @Override
+    public List<MemberDTO> getAllMembers() {
+        // Fetch all members from the repository
+        List<Member> members = memberRepository.findAll();
+
+        // Convert the list of Member entities to MemberDTOs
+        return members.stream()
+                .map(this::entityToDTO)  // Using the entityToDTO method to convert each member
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<CustomerListDTO> getAllCustomers() {
+        return memberRepository.findAll().stream()
+                .map(this::memberToCustomerDTO)
+                .collect(Collectors.toList());
+    }
+
+}
---
+++ seoulQuestFrontend_1206/src/components/products/ReadComponent.js
@@ -0,0 +1,249 @@
+import { ShoppingCart, HeartIcon } from 'lucide-react';
+import { Badge } from 'antd';
+import CartComponent from '../menus/CartComponent';
+import useCustomCart from '../../hooks/useCustomCart';
+import useCustomLogin from '../../hooks/useCustomLogin';
+import { getOne } from '../../api/productsApi';
+import useCustomFav from '../../hooks/useCustomFav';
+import ReviewsSection from '../review/ReviewsSection';
+import { API_SERVER_HOST, deleteProductOne, putProductOne, getProductItemReview } from '../../api/reviewApi';
+import { StarFilled, StarOutlined } from '@ant-design/icons';
+import ProductPolicy from './ProductPolicy';
+import { useEffect, useState } from 'react';
+
+const initState = {
+  pno: 0,
+  pname: '',
+  pdesc: '',
+  pprice: 0,
+  pqty: 0,
+  shippingFee:0,
+  uploadFileNames: [],
+};
+
+const host = API_SERVER_HOST;
+
+const ReadComponent = ({ pno }) => {
+  const [product, setProduct] = useState(initState);
+  const [fetching, setFetching] = useState(false);
+  const [currentImage, setCurrentImage] = useState(0);
+  const [cartVisible, setCartVisible] = useState(false);
+  const [selectedQuantity, setSelectedQuantity] = useState(0);
+  const { changeCart, cartItems } = useCustomCart();
+  const { loginState } = useCustomLogin();
+  const { favItems, changeFav, refreshFav } = useCustomFav();
+  const [reviewAvg, setReviewAvg] = useState(0)
+  const [reviews, setReviews] = useState([]);
+  const [refresh, setRefresh] = useState(false)
+  const [detailsVisible, setDetailsVisible] = useState(false);
+
+  const calculateAverage = (reviews) => {
+    if (reviews.length === 0) return 0;
+    const sum = reviews.reduce((acc, review) => acc + review.rating, 0);
+    return sum / reviews.length;
+  };
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    setFetching(true);
+    // Product 데이터 가져오기
+    getOne(pno).then((productData) => {
+        setProduct(productData);
+        setFetching(false);
+    });
+    console.log(product)
+    // Review 데이터 가져오기
+    console.log(loginState.email);
+    getProductItemReview(pno).then((reviews) => {
+        console.log(reviews);
+        setReviews(reviews);
+        setReviewAvg(calculateAverage(reviews))
+    });
+  }, [pno, refresh]);
+
+  const handleAddToCart = () => {
+    if (!selectedQuantity) {
+      alert('Please select a quantity.');
+      return;
+    }
+
+    const existingItem = cartItems.find((item) => item.pno === product.pno);
+    if (existingItem) {
+      if (!window.confirm('This item is already in the cart. Do you want to add it again?')) return;
+    }
+
+    changeCart({
+      email: loginState.email,
+      pno: product.pno,
+      pqty: selectedQuantity + (existingItem?.pqty || 0),
+    });
+
+    setCartVisible(true);
+  };
+
+  const handleAddToFavorites = async () => {
+    if (!loginState.email) {
+      alert('Please log in to add favorites.');
+      return;
+    }
+
+    const isAlreadyFavorite = favItems.some((item) => item.pno === product.pno);
+    if (isAlreadyFavorite) {
+      alert('You already liked this product!');
+      return;
+    }
+
+    try {
+      await changeFav({ email: loginState.email, pno: product.pno });
+      alert('Product added to favorites!');
+      refreshFav();
+    } catch (error) {
+      console.error('Failed to add favorite:', error);
+      alert('Could not add to favorites. Please try again.');
+    }
+  };
+
+  return (
+    <div className="min-h-screen py-12 px-6 lg:px-32 relative">
+      <div className="flex flex-col lg:flex-row lg:space-x-12">
+        {/* Left Section: Image Gallery */}
+        <div className="w-full lg:w-[450px] flex flex-col items-center space-y-6">
+          <div className="w-full h-64 md:h-[450px] lg:h-[650px]">
+            <img
+              src={`${host}/api/products/view/${product.uploadFileNames[currentImage]}`}
+              alt={product.pname}
+              className="w-full h-full object-cover"
+            />
+          </div>
+          <div className="flex space-x-3 overflow-x-auto w-full">
+            {product.uploadFileNames.map((image, index) => (
+              <button
+                key={index}
+                onClick={() => setCurrentImage(index)}
+                className={`w-20 h-20 overflow-hidden ${currentImage === index ? 'ring-2 ring-blue-500' : ''
+                  }`}
+              >
+                <img
+                  src={`${host}/api/products/view/${image}`}
+                  alt={`${product.pname} thumbnail`}
+                  className="w-full h-full object-cover"
+                />
+              </button>
+            ))}
+          </div>
+        </div>
+
+        {/* Right Section: Product Details */}
+        <div className="lg:w-1/2 space-y-6">
+          <h1 className="text-2xl md:text-4xl font-light text-gray-900">{product.pname}</h1>
+          <div className="flex items-center space-x-2">
+            {[...Array(5)].map((_, star) => 
+            (
+            <span key={star}>
+              {reviewAvg >= star+1 ? (
+                        <StarFilled className="text-yellow-400 text-xl" />
+                    ) : (
+                        <StarOutlined className="text-gray-300 text-xl" />
+                    )}
+              </span>
+            )
+            )}
+            <span className="text-gray-600">({reviewAvg}) {reviews.length} reviews</span>
+          </div>
+          <p className="text-2xl text-gray-900">₩{product.pprice.toLocaleString()}</p>
+          <Badge
+            count={product.shippingFee ? `Shipping Fee ₩${product.shippingFee}` : "Free Shipping"}
+            style={{
+              backgroundColor: product.shippingFee ? "#718C5A" : "#0097A7", // 유료 배송과 무료 배송에 따라 색상 변경
+              color: "#fff",
+              padding: "0 10px",
+              borderRadius: "5px",
+            }}
+          />
+          <p className="text-gray-700 mb-6">{product.pdesc}</p>
+
+          {/* Quantity Selector */}
+          <div className="flex items-center space-x-4">
+            <label htmlFor="quantity" className="text-gray-700">
+              Quantity
+            </label>
+            <input
+              type="number"
+              id="quantity"
+              min="1"
+              max={product.pqty}
+              value={selectedQuantity}
+              onChange={(e) => setSelectedQuantity(Number(e.target.value))}
+              className="border rounded-lg p-2 w-20"
+            />
+          </div>
+
+          {/* Action Buttons */}
+          <div className="flex space-x-4">
+            <button
+              onClick={handleAddToCart}
+              className="flex-1 bg-stone-400 hover:bg-stone-600 text-white py-3 rounded-lg flex items-center justify-center"
+            >
+              <ShoppingCart className="mr-2" />
+              Add to Cart
+            </button>
+            <button
+              onClick={handleAddToFavorites}
+              className="flex-1 border text-gray-700 p-1 rounded-lg flex items-center justify-center hover:bg-gray-100"
+            >
+              <HeartIcon className=" text-red-500" />
+              Add to Favorites
+            </button>
+          </div>
+
+           {/* Product Details */}
+           <div className="mt-10 bg-gray-100 p-6 rounded-lg">
+            <div className="flex justify-between items-center">
+              <h2 className="text-lg font-semibold text-gray-900">
+                Product Policies
+              </h2>
+              <button
+                onClick={() => setDetailsVisible(!detailsVisible)}
+                className="text-gray-500 hover:text-gray-700 text-2xl"
+              >
+                {detailsVisible ? "-" : "+"}
+              </button>
+            </div>
+            {detailsVisible && <ProductPolicy />}
+          </div>
+        </div>
+      </div>
+
+      {/* Cart Toggle Button */}
+      <div className="fixed top-24 right-10">
+        <Badge count={cartItems.length} offset={[0, 10]}>
+          <button
+            onClick={() => setCartVisible(!cartVisible)}
+            className="bg-stone-400 hover:bg-stone-600 text-white p-3 rounded-full shadow-lg flex items-center justify-center"
+          >
+            <ShoppingCart className="h-6 w-6" />
+          </button>
+        </Badge>
+      </div>
+
+      {/* Cart Drawer */}
+      <div
+        className={`fixed top-0 right-0 h-[70%] w-96 mt-40 p-6 overflow-auto transform ${cartVisible ? 'translate-x-0' : 'translate-x-full'
+          } transition-transform duration-300`}
+      >
+        <CartComponent stockQty={product.pqty} />
+      </div>
+      {/* Reviews Section */}
+      <div className="mt-5">
+        <ReviewsSection 
+            refresh={refresh}
+            setRefresh={setRefresh}
+            reviews={reviews}
+            putOne={putProductOne} 
+            deleteOne={deleteProductOne}/>
+      </div>
+    </div>
+  );
+};
+
+export default ReadComponent;
